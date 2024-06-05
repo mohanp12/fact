5f45ca13cf7a5cbc9ab821e47b07b0a0eb905f42
Abhijith Shivaswamy <abs@abhijith.page>
2024-06-05 19:34:43 +0530

    improve

--- src/Fact.js
+++ src/Fact.js
@@ -1,31 +1,45 @@
+import { useState } from 'react'
 export default function Fact() {
+  const [ result, setResult] = useState(0)
+  const [bold, setBold] = useState(false)
   return (
-  <>
-  <h1>FACTORIAL CALCULATOR:</h1><br /><br />
-  <label htmlFor='ipNum'>Enter a Number greater than zero: </label>
-  <input type="number" id="ipNum" name="ipNum" />
-  <button type="button" onClick={Factcalc}>Calculate</button><br></br>
-  <p id = "result"></p>
-  </>
+      <div>
+        <h1>FACTORIAL CALCULATOR:</h1><br /><br />
+        <label>Enter a Number greater than zero: </label>
+        <input type="checkbox" onChange={(e) => setBold(e.target.checked)}/>
+        <input type="number" onChange={(e) => {
+          if(e.target.value === "") return
+          let num = parseInt(e.target.value)
+          if(num < 0) {
+            alert("Please enter a number greater than zero")
+            setResult(0)
+            e.target.value = 0
+          } else {
+            setResult(factCalc(num))}
+        }
+        } />
+        <ShowResult result={result} bold={bold}></ShowResult>
+      </div>
   )
 }
 
+function ShowResult(props){
+  const { result, bold } = props
+  let text = ""
+  if(result === 0) text =  "Hey, you got a ZERO result!"
+  else text = `The factorial of the number is ${result}`
+  return (
+      <p style={{fontWeight: bold ? "bold" : "normal"}}>{text}</p>
+  )
+}
 
-export function Factcalc() {
-  let ip = document.getElementById('ipNum').value;
-  let fact = ip;
-  let n = ip;
-  if(n > 0){
-    while (n > 1) {
-      fact = fact * (n-1);
-      n--;
-    }
-    document.getElementById("result").innerHTML= " Factorial is " + fact;
+function factCalc(input) {
+  let fact = input;
+  let n = input;
+  if(n === 0) return 1
+  while (n > 1) {
+    fact = fact * (n-1);
+    n--;
   }
-  else if(n == 0){
-    document.getElementById("result").innerHTML= "Factorial is " + 1;
-  }
-  else{
-    document.getElementById("result").innerHTML= "Please Enter Number Greater than Zero";
-  }
+  return fact
 }
